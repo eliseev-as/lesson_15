f9dede316c2056639f84510c478e7638d229e0b8
a.eliseev <AEliseev_vo@inno.tech>
2024-09-03 20:44:37 +0300

    Exercise 1

---
+++ exercise_1.py
@@ -0,0 +1,14 @@
+class Transport:
+
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+    def get_info(self):
+        return f'Название автомобиля: {self.name}. Скорость: {self.max_speed}. Пробег: {self.mileage}.'
+
+
+autobus = Transport('Renault Logan', 180, 12)
+
+print(autobus.get_info())
